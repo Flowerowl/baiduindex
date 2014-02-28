3eaa1cb93fce7c4378501cbc9751386ab4ccd477
yuzhe <lazynightz@gmail.com>
2014-02-28 20:08:36 +0800

    抓取近期30天

--- index.py
+++ index.py
@@ -20,9 +20,24 @@ INDEX = 'http://index.baidu.com/?tpl=trend&word=%s'
 def createxls(result):
     wbk = xlwt.Workbook(encoding='utf-8', style_compression=0)
     sheet = wbk.add_sheet('sheet 1', cell_overwrite_ok=True)
+    title = ['ID', '电影名', '年份', '评分', '评论数', '地址', '整体搜索指数',
+            '移动搜索指数', '整体同比', '整体环比', '移动同比', '移动环比'
+    ]
+    for i, t in enumerate(title):
+        sheet.write(1, i, t)
     for i, row in enumerate(result):
-        for j, col in enumerate(row.values()):
-            sheet.write(i, j, col)
+        sheet.write(i+2, 0, row['movie_id'])
+        sheet.write(i+2, 1, row['title'])
+        sheet.write(i+2, 2, row['year'])
+        sheet.write(i+2, 3, row['rating'])
+        sheet.write(i+2, 4, row['comments'])
+        sheet.write(i+2, 5, row['url'])
+        sheet.write(i+2, 6, row['ztsszs'])
+        sheet.write(i+2, 7, row['ydsszs'])
+        sheet.write(i+2, 8, row['zttb'])
+        sheet.write(i+2, 9, row['zthb'])
+        sheet.write(i+2, 10, row['ydtb'])
+        sheet.write(i+2, 11, row['ydhb'])
     wbk.save('baiduindex.xls')
 
 def login(times, title):
@@ -38,10 +53,16 @@ def login(times, title):
 def index(times, title):
     index = {}
     login(times, title)
-    for i, elem in enumerate(gl_driver.find_elements_by_class_name("ftlwhf")):
-        index[i] = elem.text
-        print elem.text
-    return index
+    try:
+        elem = gl_driver.find_element_by_class_name('gColor1')
+        elem.send_keys(Keys.RETURN) # 选中30天
+        for i, elem in enumerate(gl_driver.find_elements_by_class_name("ftlwhf")):
+            index[i] = elem.text
+            print elem.text
+        return index
+    except:
+        print '未找到电影'
+        return [None for i in range(12)]
 
 def top250():
     result = []
@@ -56,7 +77,15 @@ def top250():
                 'rating' : movie.em.text,
                 'comments' : movie.find(attrs={'headers':'m_rating_num'}).text.strip()
             })
-            result[-1].update(index(int(movie.find(attrs={'class':'m_order'}).text.strip()), movie.a.text.split('/')[0].strip()))
+            #(登陆次数, 电影名)
+            print 'id:',result[-1]['movie_id']
+            index_result = index(int(movie.find(attrs={'class':'m_order'}).text.strip()), movie.a.text.split('/')[0].strip())
+            result[-1]['ztsszs'] = index_result[6]
+            result[-1]['ydsszs'] = index_result[7]
+            result[-1]['zttb'] = index_result[8]
+            result[-1]['zthb'] = index_result[9]
+            result[-1]['ydtb'] = index_result[10]
+            result[-1]['ydhb'] = index_result[11]
     createxls(result)
 
 top250()
